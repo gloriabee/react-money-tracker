2c3f4d1b7c80ff3a622fb13df37edc275e48c73f
figmabebe <nawgloriawinnyunt_n@cmu.ac.th>
2024-04-15 00:07:24 +0700

    responsive design fixing 2

--- src/components/Overview.js
+++ src/components/Overview.js
@@ -6,19 +6,19 @@ export default function Overview() {
      <h1 className="text-2xl font-bold mb-2 text-white">Monthly Overview</h1>
      <div className="flex flex-col md:flex-row justify-between items-center">
       {/* Balance */}
-      <div className="bg-cardBg text-cardText py-2 px-9">
+      <div className="bg-cardBg text-cardText py-2 px-9 mb-4 md:mb-0 md:mr-4">
         <h3 className="text-lg font-bold ">Balance</h3>
         <p>$ 4000</p>
       </div>
 
       {/* Income */}
-      <div className="bg-cardBg text-cardText py-2 px-9">
+      <div className="bg-cardBg text-cardText py-2 px-9 mb-4 md:mb-0 md:mr-4">
             <h3 className="text-lg font-bold ">Income</h3>
             <p className='text-green-400'>$ 4000</p>
       </div> 
 
       {/* Expenses */}
-      <div className="bg-cardBg text-cardText py-2 px-9">
+      <div className="bg-cardBg text-cardText py-2 px-9 mb-4 md:mb-0 md:mr-4">
             <h3 className="text-lg font-bold ">Expenses</h3>
             <p className='text-red-400'>$ 4000</p>
       </div>
